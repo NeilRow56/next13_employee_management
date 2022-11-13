6b14ae05b6ec4167bdd78c759305f4716e375315
Neil <neilrowland56@yahoo.com>
2022-11-13 08:07:38 +0000

    Added display of AddUserForm and UpdateUserForm

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,17 @@
-import { BiUserPlus, BiX, BiCheck } from "react-icons/bi";
+"use client"
+
+import { useState } from 'react'
+import { BiUserPlus } from "react-icons/bi";
 import Form from "./Form";
 import Table from "./Table";
 
 export default function Home() {
+
+  const [visible, setVisible ] = useState(false)
+  const handler = () => {
+    setVisible(!visible)
+  }
+
   return (
     <div >
       <main className="py-5">
@@ -11,7 +20,9 @@ export default function Home() {
         </h1>
       <div className="container mx-auto flex justify-between py-5 border-b">
         <div className="left flex-gap-3">
-          <button className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:text-gray-800 hover:border-indigo-500">
+          <button
+          onClick={handler}
+          className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:text-gray-800 hover:border-indigo-500">
             Add Employee
             <span className='px-2'><BiUserPlus size={23}></BiUserPlus></span>
           </button>
@@ -19,9 +30,9 @@ export default function Home() {
 
       </div>
        {/* Collapsable Form */}
-       <div className="container mx-auto">
-          <Form />
-       </div>
+       
+          { visible ? <Form /> : <></>}
+       
       
         
 
